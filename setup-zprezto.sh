c8423090c7987c1fb741de9a2e608adfc4202a40
Jason Brewer <jbrewer@Jasons-MacBook-Air.local>
2017-01-24 23:16:22 -1000

    Add zpreztorc

--- setup-zprezto.sh
+++ setup-zprezto.sh
@@ -1,7 +1,7 @@
 #!/usr/local/bin/zsh
 
 # Exit on any error
-set -e
+# set -e
 
 DOT_ROOT=~/dotfiles/zprezto
 
@@ -9,7 +9,30 @@ echo Zshrc
 rm ~/.zshrc
 ln -s $DOT_ROOT/zshrc ~/.zshrc
 
+echo Zpreztorc
+rm ~/.zpreztorc
+ln -s $DOT_ROOT/zpreztorc ~/.zpreztorc
+
 echo Prompt
 ln -s $DOT_ROOT/prompt_brewer_setup ~/.zprezto/modules/prompt/functions/
 
-echo Note: Still need to setup zpreztorc (see wiki)
+echo "-----------Diff-----------"
+diff ~/dotfiles/zprezto/zpreztorc ~/.zprezto/runcoms/zpreztorc
+cat <<-eos
+----------------------
+Ensure these are the only differences in above diff:
+  diff ~/dotfiles/zprezto/zpreztorc ~/.zprezto/runcoms/zpreztorc
+
+'completion' \ 
++  'homebrew' \ 
++  'osx' \ 
++  'git' \ 
++  'fasd' \ 
++  'python' \ 
++  'syntax-highlighting' \ 
++  'history-substring-search' \ 
+'prompt'
+
++# zstyle ':prezto:module:prompt' theme 'sorin'
++zstyle ':prezto:module:prompt' theme 'brewer'
+eos
