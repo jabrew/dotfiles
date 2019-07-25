db145bc31e9146144cce300d322927241a9586a1
jbrewer <jbrewer@snapchat.com>
2019-07-24 19:10:18 -0700

    Major update to mac setup guide

--- setup.sh
+++ setup.sh
@@ -30,12 +30,62 @@ brew install reattach-to-user-namespace
 brew install cmake
 "
 
+cat << EOM
+Neovim:
+xcode-select --install
+brew install --head neovim
+pip install --user neovim
+pip3 install --user neovim
+pip3 insall jedi pandas scikit-learn numpy scipy
+
+Install dein to ~/.nvim/dein
+
+Glrnvim:
+brew cask install alacritty
+mkdir ~/bin
+cd ~/bin
+
+clone repo
+Make following edits to main.rs:
++    let path = env::current_dir().unwrap();
++    command.arg("--working-directory");
++    command.arg(path.into_os_string());
+     command.arg("--class");
+
+Set ~/bin/gnv to:
+#!/bin/sh
+~/bin/glrnvim/target/release/glrnvim "$@"
+
+ln -s ~/VimConfig/glrnvim.yml ~/Library/Preferences/
+EOM
+
 echo "
-Plugin installs:
-tmux-copycat
+Other software (Mac)
+
+brew install fzf
+
+Hammerspoon
+- Run hammerspoon setup script ~/dotfiles/setup-hammerspoon.sh
+- Run hs.ipc.cliInstall() from console
+
+Karabiner elements:
+  - Application -> Right Command
+  - Caps lock -> F19
+  - Right option -> right command (only on MS Natural)
+  - Right command -> right option (only on MS Natural)
+
+Font: Source Code Pro for Powerline
+  mkdir ~/bin/powerline; cd ~/bin/powerline
+  git clone https://github.com/powerline/fonts.git
+
+Ctags:
+brew tap universal-ctags/universal-ctags
+brew install --with-jansson universal-ctags/universal-ctags/universal-ctags
+ln -s /usr/local/opt/universal-ctags/bin/ctags ~/bin/ctags
+# Ensure `ctags --list-features` includes json
 "
 
 echo "
-Links:
-ln -s ~/VimConfig/glrnvim.yml ~/Library/Preferences/
+Plugin installs:
+tmux-copycat
 "
